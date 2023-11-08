8b3923fb2e6412bd7792d2ce4011c524a1b9c664
Alex <format37@gmail.com>
2023-11-09 00:08:30 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -9,29 +9,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def record_stream(segment_duration=600):  # segment_duration is in seconds (10 minutes)
-    # Define the command to get the stream URL
     logger.info("Recording started.")
+    # Define the command to get the stream URL
     streamlink_cmd = "streamlink https://www.1tv.ru/live best -O"
-    
+
     project = os.environ.get('PROJECT')
     os.makedirs(f"data/audio/{project}", exist_ok=True)
-
-    # Get the current Unix timestamp
-    timestamp = int(time.time())
-
+    
     # Define the FFmpeg command to extract audio and split it into segments
-    
+    # ffmpeg_cmd = ("ffmpeg -i pipe:0 -f segment -segment_time {duration} "
+    #               "-c copy -vn -strftime 1 'output_%Y-%m-%d_%H-%M-%S.mp3'").format(duration=segment_duration)
     ffmpeg_cmd = ("ffmpeg -i pipe:0 -f segment -segment_time {duration} "
-                "-vn -ar 44100 -ac 2 -b:a 192k -strftime 1 "
-                "'data/audio/{project}/{timestamp}_%03d.mp3'").format(duration=segment_duration, timestamp=timestamp, project=project)
-               #"'data/audio/{project}/{timestamp}.mp3'").format(duration=segment_duration, timestamp=timestamp, project=project)
-    
+              "-vn -ar 44100 -ac 2 -b:a 192k -strftime 1 'data/audio/{project}/%Y-%m-%d_%H-%M-%S.mp3'").format(duration=segment_duration, project=project)
+
+    logger.info("Running streamlink command: %s", ffmpeg_cmd)
+
     # Start the Streamlink process
     streamlink_process = subprocess.Popen(shlex.split(streamlink_cmd), stdout=subprocess.PIPE)
     
     # Start the FFmpeg process
     ffmpeg_process = subprocess.Popen(shlex.split(ffmpeg_cmd), stdin=streamlink_process.stdout)
-
+    
     try:
         # Wait for the FFmpeg process to finish (it won't if it's a 24/7 stream)
         ffmpeg_process.wait()
